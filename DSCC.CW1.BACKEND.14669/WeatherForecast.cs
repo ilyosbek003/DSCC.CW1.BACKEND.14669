bf3b4f5440c8a41a6496b0606e8c320941d0823c
ilyosbek003 <isheraliyev003@gmail.com>
2024-11-01 00:14:09 +0500

    Add project files.

---
+++ DSCC.CW1.BACKEND.14669/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DSCC.CW1.BACKEND._14669
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
